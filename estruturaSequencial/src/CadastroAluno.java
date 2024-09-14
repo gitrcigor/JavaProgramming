05251e340dc68b65c33506126f50d7412a653db6
Igor Calixto <igorrc23@live.com>
2024-09-14 19:56:53 -0300

    estrutura sequencial execicio 01

---
+++ estruturaSequencial/src/CadastroAluno.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+
+public class CadastroAluno {
+
+    public static void main(String[] args) {
+
+        String nome;
+        String endereco;
+        String telefone;
+        int matricula;
+        int cont = 0;
+
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Insira a matrícula do aluno: ");
+        matricula = teclado.nextInt();
+
+
+        while (matricula != 0) {
+
+            System.out.println("Nome do aluno: ");
+            nome = teclado.next();
+
+            System.out.println("Endereço do aluno: ");
+            endereco = teclado.next();
+
+            System.out.println("Telefone do aluno: ");
+            telefone = teclado.next();
+
+
+
+            cont++;
+
+            System.out.println("Matrícula do próximo aluno: ");
+            matricula = teclado.nextInt();
+        }
+
+
+    }
+}
