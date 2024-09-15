e109838576c56023c7d30ba4ab77878609ea5e75
Igor Calixto <igorrc23@live.com>
2024-09-15 16:46:34 -0300

    AvaliacaoNota if/else

--- estruturaDeDecisao/src/AvaliacaoNota.java
+++ estruturaDeDecisao/src/AvaliacaoNota.java
@@ -1,8 +1,34 @@
+import java.util.Scanner;
+
 public class AvaliacaoNota {
 
     public static void main(String[] args) {
 
+        double nota;
 
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Insira sua nota de 0 a 100 para saber o conceito: ");
+        nota = teclado.nextDouble();
+
+        if ((nota >= 0) && (nota <= 100)){
+
+            if (nota >= 90)
+                System.out.println("Nota excelente!");
+            else if (nota >= 70)
+                System.out.println("Conceito bom!");
+            else if (nota >= 50)
+                System.out.println("Conceito regular!");
+            else if (nota >= 0)
+                System.out.println("Nota insuficiente!");
+
+
+        }
+        else {
+        System.out.println("Nota inválida!");
+        AvaliacaoNota.main(null);
+
+        }
 
     }
 
