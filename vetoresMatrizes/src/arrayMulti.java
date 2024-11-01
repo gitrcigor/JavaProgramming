e562bde6529f0fe541a4f2eedd63fb2984f4da7d
Igor Calixto <igorrc23@live.com>
2024-11-01 19:35:06 -0300

    [Matriz] array multi...

--- vetoresMatrizes/src/arrayMulti.java
+++ vetoresMatrizes/src/arrayMulti.java
@@ -12,12 +12,27 @@
 // numeros [2] [1] = 6;
 // numeros [2] [2] = 32;
 
-// Cheatsheet programação
-// https://books.goalkicker.com/
+// Cheatsheet programação Java
+// https://books.goalkicker.com/JavaBook/
 
 public class arrayMulti {
     public static void main(String[] args) {
 
+        // Declaração
+        int outros_numeros[][];
+
+        // Declaração e definição de tamanho
+        int numeros[][] = new int[3][3];
+
+        // Declarar, definir o tamanho e inicializar
+        int mais_numeros[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        // Declarar uma matriz informando somente as linhas
+        int matriz[][] = new int[2][];
+        matriz[0] = new int[5];
+        matriz[1] = new int[3];
+
+        int nova_matriz[][] = {{1, 2}, {4, 5, 6, 6, 7, 8}, {9, 10, 11}};
 
 
     }
