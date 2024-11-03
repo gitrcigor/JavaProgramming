3d5ae884c2080447fa307ee7d40efc2a745f74ff
Igor Calixto <igorrc23@live.com>
2024-11-02 22:58:47 -0300

    [Funcoes] algoritmos para estudar funcoes/methods

---
+++ funcoes/src/funcaoSemParam.java
@@ -0,0 +1,17 @@
+// Estudando sobre o conceito de funções em Java
+
+// Chamada de um função simples sem passagem de parâmetro
+
+public class funcaoSemParam {
+
+    static void meuMetodo() {
+        System.out.println("\nFunção chamada e retornada corretamente!");
+    }
+
+
+    public static void main(String[] args) {
+
+        meuMetodo();
+
+    }
+}
