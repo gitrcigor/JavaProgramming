e47a72e9825d9b64c1be51414e577f069be91ba5
Igor Calixto <igorrc23@live.com>
2025-08-10 22:18:00 -0300

    [POO]Exercício 02 classes abstatas

--- src/br/com/geekuniversity/secao12/exercitando/execicio01/ProgramaPrincipalParaClasseFigura.java
+++ src/br/com/geekuniversity/secao12/exercitando/execicio01/ProgramaPrincipalParaClasseFigura.java
@@ -1,5 +1,12 @@
 package src.br.com.geekuniversity.secao12.exercitando.execicio01;
-
+/*
+Exercício retirado do google
+* 1. Figuras Geométricas:
+Crie uma classe abstrata Figura com um método abstrato calcular_area().
+Crie subclasses concretas como Retangulo, Circulo e Triangulo que herdam de Figura e implementam o método calcular_area() de acordo com suas fórmulas específicas.
+*
+*
+* */
 public class ProgramaPrincipalParaClasseFigura {
     public static void main(String[] args) {
         System.out.print("\n--- Figura Retângulo ---");
---
+++ src/br/com/geekuniversity/secao12/exercitando/exercicio02/Pagamento.java
@@ -0,0 +1,15 @@
+package src.br.com.geekuniversity.secao12.exercitando.exercicio02;
+
+public abstract class Pagamento {
+
+    public int valor;
+    public String titular;
+
+    public Pagamento(int valor, String titular){
+        this.valor = valor;
+        this.titular = titular;
+    }
+
+    public abstract int processarPagamento();
+
+}
